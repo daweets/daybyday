dcc99b38fd7f83583ef83b1256449c665277c538
Dawit Solomon <dawits2003@gmail.com>
2023-10-16 15:51:34 -0700

    hardcoded twillio set-up; next step: in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import pytz
+from twilio.rest import Client
 
 def get_google_calendar():
     scopes = ['https://www.googleapis.com/auth/calendar.readonly']
@@ -18,11 +19,31 @@ def get_google_calendar():
     return service
 
 def get_events(calendar_service):
-    pst = pytz.timezone('America/Los Angeles')
+    pst = pytz.timezone('America/Los Angeles') 
+    now = datetime.now(pst)
+    end_of_day = datetime(now.year, now.month, now.day, 23, 0, 0)
+
+    events_result = calendar_service.events().list(
+        calendarId = 'primary',
+        timeMin=now.isoformat(),
+        timeMax=end_of_day.isoformat(),
+        singleEvents = True,
+        orderBy='startTime'
+    ).execute()
+
+    events = events_result.get(
+        'items',
+        []
+    )
+
+    return events
+
+def send_schedule_text(schedule):
+    #Twilio Set-up
+    TWILIO_SID = ''
 
 
-
-def main ():
+def main():
     cal_service = get_google_calendar()
     schedule = get_events(cal_service)
     send_schedule_text(schedule)
---
+++ send_sms.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = "ACf7348fc95188d816dd47a5c9136f391c"
+auth_token  = "31a62d8f5fa8a041226415bd3781fe02"
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+    .create(
+         body='thats cool',
+         from_='++18336481841',
+         to='+15106405848'
+     )
+
+print(message.sid)
